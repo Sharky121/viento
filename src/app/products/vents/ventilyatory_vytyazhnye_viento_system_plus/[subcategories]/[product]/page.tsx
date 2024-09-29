5eef5aa0c551c1215709f2591729f74fa46ef554
Sharky <loginov@2cherryline.ru>
2024-09-29 23:44:38 +0300

    feat: Добавляет фото

--- src/app/products/vents/ventilyatory_vytyazhnye_viento_system_plus/[subcategories]/[product]/page.tsx
+++ src/app/products/vents/ventilyatory_vytyazhnye_viento_system_plus/[subcategories]/[product]/page.tsx
@@ -202,7 +202,11 @@ const Products = [
             "image_4.png",
             "image_5.png",
             "image_6.png",
-            "image_7.png"
+            "image_7.png",
+            "image_8.png",
+            "image_9.png",
+            "image_10.png",
+            "image_11.png"
         ]
     },
     {
@@ -336,7 +340,7 @@ const Products = [
             },
         ],
         "images": [
-            "image_1.png",
+            "index.png",
             "image_2.png",
             "image_3.png"
         ]
