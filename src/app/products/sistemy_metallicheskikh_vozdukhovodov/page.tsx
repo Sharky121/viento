e96c7a08d0aabb6be77c23cbbbea3bafc846fecf
Sharky <loginov@2cherryline.ru>
2024-11-20 12:20:21 +0300

    feat: Воздуховоды

---
+++ src/app/products/sistemy_metallicheskikh_vozdukhovodov/page.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import Image from "next/image";
+import { v4 as uuidv4 } from 'uuid';
+const { slugify } = require('transliter');
+
+async function getData() {
+    const response = await fetch(`${process.env.HOST}/api/categories`);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return response.json();
+}
+
+export default async function Page() {
+    const data = await getData();
+    
+    const products = data.find(({slug}: {slug: string}) => slug === "sistemy_metallicheskikh_vozdukhovodov");
+
+    return (
+        <>
+            <div className="page-main__content main-content">
+                <a className="main-content__back" href="/products">Назад</a>
+                
+                <div className="main-content__header content-header">
+                    <h2 className="content-header__title">Каталог</h2>
+                    <p className="content-header__name">Системы металлических воздуховодов</p>
+                </div>
+
+                <section className="main-content__product-cards product-cards">
+                    <ul className="product-cards__list">
+                        {products.subcategory.map(({title, slug}: { title: string, slug: string }, index: number) => (
+                            <li key={index + uuidv4()} className="product-cards__item product-card">
+                                <div className="product-card__img">
+                                    <Image src={`/images/sistemy_metallicheskikh_vozdukhovodov/${slug}/index.png`} width="180" height="180" alt="Фото продукта"/>
+                                </div>
+                    
+                                <h3 className="product-card__title">{title}</h3>
+                                <Link className="product-card__btn btn btn--outline" href={`/products/sistemy_metallicheskikh_vozdukhovodov/${slug}`}>Подробнее</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            </div>
+        </>
+    )
+}
