2cffe9414f52bc033059f2cf8bf265ac3df3e1d1
Sharky <loginov@2cherryline.ru>
2024-11-29 00:38:40 +0300

    fix: Возвращает баннер tus

---
+++ src/app/products/ekrany_dekorativnye_dlya_radiatorov/page.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import Image from "next/image";
+import { v4 as uuidv4 } from 'uuid';
+
+async function getData() {
+    const response = await fetch(`${process.env.HOST}/api/shields`);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return response.json();
+}
+
+export default async function Page() {
+    const data = await getData();
+
+    return (
+        <>
+            <div className="page-main__content main-content">
+                <a className="main-content__back" href="/products">Назад</a>
+                
+                <div className="main-content__header content-header">
+                    <h2 className="content-header__title">Каталог</h2>
+                    <p className="content-header__name">Экраны декоративные для радиаторов</p>
+                </div>
+                
+                <section className="main-content__product-cards product-cards">
+                    <ul className="product-cards__list">
+                        {data.map(({title, slug}: {title: string, slug: string }, index: number) => (
+                            <li key={index + uuidv4()} className="product-cards__item product-card">
+                                <div className="product-card__img">
+                                    <Image src={`/images/ekrany_dekorativnye_dlya_radiatorov/${slug}/index.png`} width="180" height="180" alt="Фото продукта"/>
+                                </div>
+                    
+                                <h3 className="product-card__title">{title}</h3>
+                                <Link className="product-card__btn btn btn--outline" href={`/products/ekrany_dekorativnye_dlya_radiatorov/${slug}`}>Подробнее</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            </div>
+        </>
+    )
+}
