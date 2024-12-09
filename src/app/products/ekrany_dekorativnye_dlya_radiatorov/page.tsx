ce125a743fab642cce19a71b8a50f504a97afec5
Sharky <loginov@2cherryline.ru>
2024-12-09 16:07:55 +0300

    feat: Убирает поле регионы из формы заявки осмотра

--- src/app/products/ekrany_dekorativnye_dlya_radiatorov/page.tsx
+++ src/app/products/ekrany_dekorativnye_dlya_radiatorov/page.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 import Image from "next/image";
 import { v4 as uuidv4 } from 'uuid';
 
-async function getData() {
-    const response = await fetch(`${process.env.HOST}/api/shields`);
+// async function getData() {
+//     const response = await fetch(`${process.env.HOST}/api/shields`);
 
-    if (!response.ok) {
-        throw new Error('Failed to fetch data')
-    }
+//     if (!response.ok) {
+//         throw new Error('Failed to fetch data')
+//     }
 
-    return response.json();
-}
+//     return response.json();
+// }
 
 export default async function Page() {
-    const data = await getData();
+    // const data = await getData();
 
     return (
         <>
@@ -26,7 +26,7 @@ export default async function Page() {
                 </div>
                 
                 <section className="main-content__product-cards product-cards">
-                    <ul className="product-cards__list">
+                    {/* <ul className="product-cards__list">
                         {data.map(({title, slug}: {title: string, slug: string }, index: number) => (
                             <li key={index + uuidv4()} className="product-cards__item product-card">
                                 <div className="product-card__img">
@@ -37,7 +37,7 @@ export default async function Page() {
                                 <Link className="product-card__btn btn btn--outline" href={`/products/ekrany_dekorativnye_dlya_radiatorov/${slug}`}>Подробнее</Link>
                             </li>
                         ))}
-                    </ul>
+                    </ul> */}
                 </section>
             </div>
         </>
