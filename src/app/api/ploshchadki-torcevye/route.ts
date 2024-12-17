42bada19963f8528e511eff450aaa990921187ca
Sharky <loginov@2cherryline.ru>
2024-12-17 17:55:31 +0300

    feat: Площадки торцевые

---
+++ src/app/api/ploshchadki-torcevye/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+import data from '../../../../data/ploshchadki_torcevye.json';
+
+export async function GET(request: Request) {
+    return NextResponse.json(data);
+}
