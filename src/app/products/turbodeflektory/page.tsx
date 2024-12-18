4199829fb5c9822a82f62b302e23248c9132f28f
Sharky <loginov@2cherryline.ru>
2024-12-18 17:39:43 +0300

    feat: Системы пластиковых воздуховодов

---
+++ src/app/products/turbodeflektory/page.tsx
@@ -0,0 +1,57 @@
+const { slugify } = require('transliter');
+
+import Gallery from "@/app/components/gallery/gallery";
+
+async function getData() {
+    const response = await fetch(`${process.env.HOST}/api/turbodeflektory`, {cache: 'no-store'});
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return response.json();
+}
+
+type ParamsType = {
+    slug: string;
+}
+
+export default async function Page({params}: {params: ParamsType}) {
+    const product = await getData();
+
+    return (
+        <>
+            <div className="page-main__content main-content">
+                <a className="main-content__back" href={`/products/`} id="back-page">Назад</a>
+                
+                <div className="main-content__header content-header">
+                    <h2 className="content-header__title">Каталог</h2>
+                    <p className="content-header__name">{product.title}</p>
+                </div>
+
+                <section className="main-content__product product">
+                    <div className="product__container">
+                        <div className="product__image">
+                            <Gallery images={product.images} url={`/images/products/turbodeflektory/${params.slug}`}/>
+                        </div>
+                        <div className="product__description product-description">
+                            <h1 className="product-description__title">
+                                {product.title}
+                            </h1>
+
+                            <ul className="product-description__list">
+                                {
+                                    product.features.map((item: string, index: string) => (
+                                        <li key={index} className="product-description__item">{item}</li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+
+                        {/* <div className={styles.productDesc} dangerouslySetInnerHTML={{__html: product.description}} /> */}
+                    </div>
+                </section>
+            </div>
+        </>
+    )
+}
