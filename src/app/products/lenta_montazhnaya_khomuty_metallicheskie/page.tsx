1cedaddd25ad0b3d7006ea062d11c9b445482369
Sharky <loginov@2cherryline.ru>
2024-12-18 18:37:43 +0300

    feat: Хомуты

--- src/app/products/lenta_montazhnaya_khomuty_metallicheskie/page.tsx
+++ src/app/products/lenta_montazhnaya_khomuty_metallicheskie/page.tsx
@@ -2,20 +2,20 @@ const { slugify } = require('transliter');
 
 import Gallery from "@/app/components/gallery/gallery";
 
-async function getData() {
-    const response = await fetch(`${process.env.HOST}/api/khomuty`, {cache: 'no-store'});
+// async function getData() {
+//     const response = await fetch(`${process.env.HOST}/api/khomuty`, {cache: 'no-store'});
 
-    if (!response.ok) {
-        throw new Error('Failed to fetch data')
-    }
+//     if (!response.ok) {
+//         throw new Error('Failed to fetch data')
+//     }
 
-    return response.json();
-}
+//     return response.json();
+// }
 
 export default async function Page() {
-    const data = await getData();
+    // const data = await getData();
 
-    const product = data[0];
+    // const product = data[0];
 
     return (
         <>
@@ -24,26 +24,26 @@ export default async function Page() {
                 
                 <div className="main-content__header content-header">
                     <h2 className="content-header__title">Каталог</h2>
-                    <p className="content-header__name">{product.title}</p>
+                    {/* <p className="content-header__name">{product.title}</p> */}
                 </div>
 
                 <section className="main-content__product product">
                     <div className="product__container">
                         <div className="product__image">
-                            <Gallery images={product.images} url={`/images/products/lenta_montazhnaya_khomuty_metallicheskie`}/>
+                            {/* <Gallery images={product.images} url={`/images/products/lenta_montazhnaya_khomuty_metallicheskie`}/> */}
                         </div>
                         <div className="product__description product-description">
                             <h1 className="product-description__title">
-                                {product.title}
+                                {/* {product.title} */}
                             </h1>
 
-                            <ul className="product-description__list">
+                            {/* <ul className="product-description__list">
                                 {
                                     product.features.map((item: string, index: string) => (
                                         <li key={index} className="product-description__item">{item}</li>
                                     ))
                                 }
-                            </ul>
+                            </ul> */}
                         </div>
 
                         {/* <div className={styles.productDesc} dangerouslySetInnerHTML={{__html: product.description}} /> */}
