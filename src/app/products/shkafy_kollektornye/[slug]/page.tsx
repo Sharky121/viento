50562f5289d862998f3fcf49d4874ab055ee42d5
Sharky121 <sharky121@github.com>
2025-05-31 22:00:19 +0300

    WIP 3

--- src/app/products/shkafy_kollektornye/[slug]/page.tsx
+++ src/app/products/shkafy_kollektornye/[slug]/page.tsx
@@ -1,5 +1,7 @@
 const { slugify } = require('transliter');
 
+import Gallery from "@/app/components/gallery/gallery";
+
 async function getData() {
     const response = await fetch(`${process.env.HOST}/api/shkafy-kollektornye`, {
         cache: 'no-store',
@@ -18,6 +20,10 @@ type ParamsType = {
 }
 
 export default async function Page({params}: {params: ParamsType}) {
+    const data = await getData();
+
+    const product = data.find(({title}: {title: string}) => slugify(title, '_') === params.slug);
+
     return (
         <>
             <div className="page-main__content main-content">
@@ -25,21 +31,25 @@ export default async function Page({params}: {params: ParamsType}) {
                 
                 <div className="main-content__header content-header">
                     <h2 className="content-header__title">Каталог</h2>
-                    <p className="content-header__name"></p>
+                    <p className="content-header__name">{product.title}</p>
                 </div>
 
                 <section className="main-content__product product">
                     <div className="product__container">
                         <div className="product__image">
-                            
+                            <Gallery images={product.images} url={`/images/products/shkafy_kollektornye/${params.slug}`}/>
                         </div>
                         <div className="product__description product-description">
                             <h1 className="product-description__title">
-                 
+                                {product.title}
                             </h1>
 
                             <ul className="product-description__list">
-
+                                {
+                                    product.features.map((item: string, index: string) => (
+                                        <li key={index} className="product-description__item">{item}</li>
+                                    ))
+                                }
                             </ul>
                         </div>
 
