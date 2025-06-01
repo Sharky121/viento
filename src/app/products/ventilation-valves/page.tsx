afe095461fa06da092f98adbd63c096192b11b00
Sharky121 <sharky121@github.com>
2025-06-01 16:36:06 +0300

    WIP 8

--- src/app/products/ventilation-valves/page.tsx
+++ src/app/products/ventilation-valves/page.tsx
@@ -16,7 +16,6 @@ async function getData() {
 }
 
 export default async function Page() {
-    const products = await getData();
     
     return (
         <>
@@ -30,16 +29,7 @@ export default async function Page() {
 
                 <section className="main-content__product-cards product-cards">
                     <ul className="product-cards__list">
-                        {products.map(({title}: { title: string }, index: number) => (
-                            <li key={index + uuidv4()} className="product-cards__item product-card">
-                                <div className="product-card__img">
-                                    <Image src={`/images/products/ventilation-valves/${slugify(title, '_')}/index.png`} width="180" height="180" alt="Фото продукта"/>
-                                </div>
-                    
-                                <h3 className="product-card__title">{title}</h3>
-                                <Link className="product-card__btn btn btn--outline" href={`/products/ventilation-valves/${slugify(title, '_')}`}>Подробнее</Link>
-                            </li>
-                        ))}
+
                     </ul>
                 </section>
             </div>
