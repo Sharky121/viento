7e292dda7357a6ba3425cf813ec40371c0759836
Sharky121 <sharky121@github.com>
2025-06-01 22:37:01 +0300

    WIP 16

--- src/app/products/lenta_montazhnaya/[category]/page.tsx
+++ src/app/products/lenta_montazhnaya/[category]/page.tsx
@@ -14,7 +14,12 @@ async function getData() {
 }
 
 export default async function Page({ params }: { params: { category: string } }) {
+    const products = await getData();
+    const product = products.find((p: any) => p.slug === params.category);
 
+    if (!product) {
+        return <div>Продукт не найден</div>;
+    }
 
     return (
         <>
@@ -23,20 +28,23 @@ export default async function Page({ params }: { params: { category: string } })
                 
                 <div className="main-content__header content-header">
                     <h2 className="content-header__title">Каталог</h2>
-                    <p className="content-header__name"></p>
+                    <p className="content-header__name">{product.title}</p>
                 </div>
 
                 <section className="main-content__product product">
                     <div className="product__container">
                         <div className="product__image">
+                            <Gallery images={product.images} url={`/images/products/lenta_montazhnaya/${product.slug}`}/>
                         </div>
                         <div className="product__description product-description">
                             <h1 className="product-description__title">
-                               
+                                {product.title}
                             </h1>
 
                             <ul className="product-description__list">
-
+                                {product.features.map((item: string, index: number) => (
+                                    <li key={index} className="product-description__item">{item}</li>
+                                ))}
                             </ul>
                         </div>
                     </div>
