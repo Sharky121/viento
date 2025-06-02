77dfc2f6e2dc1217400627c8019de01d2cd00ee9
Sharky121 <sharky121@github.com>
2025-06-03 00:18:44 +0300

    WIP 3

--- src/app/products/ekrany_dekorativnye_dlya_radiatorov/[slug]/page.tsx
+++ src/app/products/ekrany_dekorativnye_dlya_radiatorov/[slug]/page.tsx
@@ -16,9 +16,7 @@ type ParamsType = {
 }
 
 export default async function Page({params}: {params: ParamsType}) {
-    const data = await getData();
 
-    const product = data.find(({slug}: {slug: string}) => slug === params.slug);
 
     return (
         <>
@@ -27,25 +25,20 @@ export default async function Page({params}: {params: ParamsType}) {
                 
                 <div className="main-content__header content-header">
                     <h2 className="content-header__title">Каталог</h2>
-                    <p className="content-header__name">{product.title}</p>
+                    <p className="content-header__name"></p>
                 </div>
 
                 <section className="main-content__product product">
                     <div className="product__container">
                         <div className="product__image">
-                            <Gallery images={product.images} url={`/images/ekrany_dekorativnye_dlya_radiatorov/${params.slug}`}/>
                         </div>
                         <div className="product__description product-description">
                             <h1 className="product-description__title">
-                                {product.title}
+                              
                             </h1>
 
                             <ul className="product-description__list">
-                                {
-                                    product.features.map((item: string, index: string) => (
-                                        <li key={index} className="product-description__item">{item}</li>
-                                    ))
-                                }
+       
                             </ul>
                         </div>
 
--- src/app/products/ekrany_dekorativnye_dlya_radiatorov/page.tsx
+++ src/app/products/ekrany_dekorativnye_dlya_radiatorov/page.tsx
@@ -13,7 +13,7 @@ async function getData() {
 }
 
 export default async function Page() {
-    const data = await getData();
+
 
     return (
         <>
@@ -22,21 +22,12 @@ export default async function Page() {
                 
                 <div className="main-content__header content-header">
                     <h2 className="content-header__title">Каталог</h2>
-                    <p className="content-header__name">Экраны декоративные для радиаторов</p>
+                    <p className="content-header__name">Экраны декоративные для радиаторов1</p>
                 </div>
                 
                 <section className="main-content__product-cards product-cards">
                     <ul className="product-cards__list">
-                        {data.map(({title, slug}: {title: string, slug: string }, index: number) => (
-                            <li key={index + uuidv4()} className="product-cards__item product-card">
-                                <div className="product-card__img">
-                                    <Image src={`/images/ekrany_dekorativnye_dlya_radiatorov/${slug}/index.png`} width="180" height="180" alt="Фото продукта"/>
-                                </div>
-                    
-                                <h3 className="product-card__title">{title}</h3>
-                                <Link className="product-card__btn btn btn--outline" href={`/products/ekrany_dekorativnye_dlya_radiatorov/${slug}`}>Подробнее</Link>
-                            </li>
-                        ))}
+
                     </ul>
                 </section>
             </div>
