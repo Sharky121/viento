b3922a76db67ad41e1b5bae630807d7cf8b6ead3
Sharky121 <sharky121@github.com>
2025-07-10 00:07:33 +0300

    WIP Пластиковые воздуховоды 1

--- src/app/products/sistemy_plastikovykh_vozdukhovodov/[category]/[subcategory]/page.tsx
+++ src/app/products/sistemy_plastikovykh_vozdukhovodov/[category]/[subcategory]/page.tsx
@@ -19,11 +19,11 @@ type ParamsType = {
 }
 
 export default async function Page({params}: {params: ParamsType}) {
-    const data = await getData();
+    // const data = await getData();
 
-    const category = data.find((p: any) => p.slug === params.category);
+    // const category = data.find((p: any) => p.slug === params.category);
 
-    const subcategory = category.items.find((p: any) => p.slug === params.subcategory);
+    // const subcategory = category.items.find((p: any) => p.slug === params.subcategory);
 
     return (
         <>
@@ -32,12 +32,12 @@ export default async function Page({params}: {params: ParamsType}) {
                 
                 <div className="main-content__header content-header">
                     <h2 className="content-header__title">Каталог</h2>
-                    <p className="content-header__name">{subcategory.title}</p>
+                    {/* <p className="content-header__name">{subcategory.title}</p> */}
                 </div>
 
                 <section className="main-content__product-cards product-cards">
                     <ul className="product-cards__list">
-                        {subcategory.items.map(({title, slug, poster}: { title: string, slug: string, poster: string }, index: number) => (
+                        {/* {subcategory.items.map(({title, slug, poster}: { title: string, slug: string, poster: string }, index: number) => (
                             <li key={index + uuidv4()} className="product-cards__item product-card">
                                 <div className="product-card__img">
                                     <Image src={`/images/products/sistemy_plastikovykh_vozdukhovodov/${params.category}/${params.subcategory}/${slugify(title, '_')}/index.png`} width="180" height="180" alt="Фото продукта"/>
@@ -46,7 +46,7 @@ export default async function Page({params}: {params: ParamsType}) {
                                 <h3 className="product-card__title">{title}</h3>
                                 <Link className="product-card__btn btn btn--outline" href={`/products/sistemy_plastikovykh_vozdukhovodov/${params.category}/${params.subcategory}/${slugify(title, '_')}`}>Подробнее</Link>
                             </li>
-                        ))}
+                        ))} */}
                     </ul>
                 </section>
             </div>
