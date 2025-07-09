f4e0d0088dc8f3aea35515b2d5bea044b87a7ef8
Sharky121 <sharky121@github.com>
2025-07-10 01:38:47 +0300

    WIP Решетки вент 2

--- src/app/api/ventilation-grilles/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-import ventilationGrilles from '@/data/ventilation-grilles.json';
-
-export async function GET() {
-    return NextResponse.json(ventilationGrilles);
-} 
