5f90b108ad79f633750f1d530e34e176355a1dd6
Sharky121 <sharky121@github.com>
2025-07-10 02:37:10 +0300

    WIP Решетки вент 6

--- src/app/products/reshetki_ventilyacionnye/[category]/[product]/page.tsx
+++ src/app/products/reshetki_ventilyacionnye/[category]/[product]/page.tsx
@@ -19,15 +19,15 @@ type ParamsType = {
 
 
 export default async function Page({ params }: { params: ParamsType }) {
-    // const data = await getData();
+    const data = await getData();
 
-    // const category = data.find((p: any) => p.slug === params.category);
+    const category = data.find((p: any) => p.slug === params.category);
 
-    // const product = category.items.find((p: any) => p.slug === params.product);
+    const product = category.items.find((p: any) => p.slug === params.product);
 
-    // if (!product) {
-    //     return <div>Продукт не найден</div>;
-    // }
+    if (!product) {
+        return <div>Продукт не найден</div>;
+    }
 
     return (
         <>
@@ -36,28 +36,28 @@ export default async function Page({ params }: { params: ParamsType }) {
                 
                 <div className="main-content__header content-header">
                     <h2 className="content-header__title">Каталог</h2>
-                    {/* <p className="content-header__name">{product.title}</p> */}
+                    <p className="content-header__name">{product.title}</p>
                 </div>
 
                 <section className="main-content__product product">
                     <div className="product__container">
                         <div className="product__image">
-                            {/* <Gallery images={product.images} url={`/images/products/reshetki_ventilyacionnye/${params.category}/${params.product}`}/> */}
+                            <Gallery images={product.images} url={`/images/products/reshetki_ventilyacionnye/${params.category}/${params.product}`}/>
                         </div>
                         <div className="product__description product-description">
                             <h1 className="product-description__title">
-                                {/* {product.title} <br /> 
+                                {product.title} <br /> 
 
                                 {product.colors.length !== 0 && (
                                     <span>Доступные цвета: {product.colors.join(', ') + '.'}
                                     </span>  
-                                )} */}
+                                )}
                             </h1>
 
                             <ul className="product-description__list">
-                                {/* {product.features.map((item: string, index: number) => (
+                                {product.features.map((item: string, index: number) => (
                                     <li key={index} className="product-description__item">{item}</li>
-                                ))} */}
+                                ))}
                             </ul>
                         </div>
                     </div>
